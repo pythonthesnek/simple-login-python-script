e71b4ba3b51b42484c97a57009efdfe620caa981
python_ <theoreticallytrash.py@gmail.com>
2024-05-13 18:13:29 +0300

    stopped working
    idk

--- login.py
+++ login.py
@@ -15,10 +15,13 @@ def write(user, passwordname):
     with open("accounts.txt", "a") as file:
         file.write("\n" + user + "/" + passwordname)
 
+read()
+
 while True:
-    read()
+
     query_user = input("Type user or create account: ")
-    h.update(query_user.encode())
+    h = hashlib.new("SHA256")
+    h.update(query_user.encode)
     query_user_hash = h.hexdigest()
 
     if query_user_hash in username[0]:
@@ -27,27 +30,31 @@ while True:
         create_user = input("user not found in the database. Do you want to create a new user? [y/n]")
         if create_user == "y":
             new_user = input("input name for new user: ")
+            h = hashlib.new("SHA256")
             h.update(new_user.encode())
             new_user_hash = h.hexdigest()
 
             new_password = input("input new password: ")
+            h = hashlib.new("SHA256")
             h.update(new_password.encode())
             new_password_hash = h.hexdigest()
 
             write(new_user_hash, new_password_hash)
-
             break
+
         else:
             print("canceling...")
             print("Done!")
             break
     
     query_password = input("Type password for " + query_user + ":")
+    h = hashlib.new("SHA256")
     h.update(query_password.encode())
     query_password_hash = h.hexdigest()
 
     if query_password_hash in username[1]:
         print("access granted!")
+        break
     elif query_password_hash not in username[1]:
         print("access denied!")
         break
